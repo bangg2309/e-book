1975e58e829cf425bd7746334abaecffe9528058
bangg2309 <tuyeu1aido@gmail.com>
2024-05-18 21:36:06 +0700

    initial project spring boot

---
+++ EBook/src/main/java/edu/vn/hcmuaf/ebook/configuration/ApplicationInitConfig.java
@@ -0,0 +1,34 @@
+package edu.vn.hcmuaf.ebook.configuration;
+
+import edu.vn.hcmuaf.ebook.entity.User;
+import edu.vn.hcmuaf.ebook.repository.UserRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
+public class ApplicationInitConfig {
+    PasswordEncoder passwordEncoder;
+
+    @Bean
+    ApplicationRunner applicationRunner(UserRepository userRepository) {
+        return args -> {
+            if (userRepository.findByUsername("admin").isEmpty()) {
+                User user = User.builder()
+                        .username("admin")
+                        .password(passwordEncoder.encode("admin"))
+                        // .roles(roles)
+                        .build();
+                userRepository.save(user);
+            }
+        };
+    }
+}
